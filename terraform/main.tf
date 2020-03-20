1a28d3e1a2b104c08060d7689540f2cfa1c96660
Jeremy Finn <jeremy.finn@gmail.com>
2020-03-19 23:35:48 -0400

    Change name of vpc module.

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,7 +6,7 @@ terraform {
   backend "s3" {}
 }
 
-module "aws-vpc" {
+module "main_vpc" {
   source            = "github.com/finnjitsu/aws-vpc"
   region            = var.region
   stack_name        = var.key
@@ -17,4 +17,18 @@ module "aws-vpc" {
   db_subnet_b_cidr  = var.db_subnet_b_cidr
   web_subnet_a_cidr = var.web_subnet_a_cidr
   web_subnet_b_cidr = var.web_subnet_b_cidr
+}
+
+
+module "jenkins" {
+  source            = "github.com/finnjitsu/jenkins"
+  region            = var.region
+  stack_name        = var.key
+  ec2_instance_type = "t3.micro"
+  root_disk_sz      = "20"
+  app_disk_sz       = "20"
+  app_subnet_a_id   = module.main_vpc.app_subnet_a_id
+  app_subnet_b_id   = module.main_vpc.app_subnet_b_id
+  web_subnet_a_id   = module.main_vpc.web_subnet_a_id
+  web_subnet_b_id   = module.main_vpc.web_subnet_b_id
 }
